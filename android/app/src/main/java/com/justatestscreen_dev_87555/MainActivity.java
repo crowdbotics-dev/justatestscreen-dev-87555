31725159030407efedffcee5e56534289ef97040
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 16:46:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/justatestscreen_dev_87555/MainActivity.java
@@ -0,0 +1,15 @@
+package com.justatestscreen_dev_87555;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "justatestscreen_dev_87555";
+  }
+}
