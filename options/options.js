4700ac82eebab5153927828da716d4dc06cd1dbb
scartag <tag@crowdbotics.com>
2023-06-23 16:53:50 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "justatestscreen-dev-87555",
-  url: "https://justatestscreen-dev-87555.botics.co",
-  api: "https://justatestscreen-dev-87555.botics.co/api/v1"
+  "name": "justatestscreen-dev-87555",
+  "url": "https://justatestscreen-dev-87555.botics.co",
+  "api": "https://justatestscreen-dev-87555.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
